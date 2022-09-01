eee0ae8395a026b5b4a3b7dd8cf351b9d77074dc
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-01 15:01:18 -0300

    intalaçao de dependicas sass e module sass

---
+++ src/pages/Cardapio/index.tsx
@@ -0,0 +1,13 @@
+import style from  './Cardapio.module.scss'
+
+const Cardapio = () => {
+ return (
+    <main>
+      <nav className={style.menu}>
+
+      </nav>
+    </main>
+ )
+}
+
+export default Cardapio
