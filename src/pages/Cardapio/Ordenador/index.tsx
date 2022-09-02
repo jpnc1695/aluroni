7375387f0a35c1644285e324f53f4414077e2a56
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-02 16:34:04 -0300

    icones para ordenador

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,7 +1,8 @@
-import style from './Ordenador.module.scss'
-import opcoes from './opcoes.json'
-import { useState } from 'react'
-import classNames from 'classnames'
+import style from './Ordenador.module.scss';
+import opcoes from './opcoes.json';
+import { useState } from 'react';
+import classNames from 'classnames';
+import {MdKeyboardArrowUp, MdKeyboardArrowDown} from 'react-icons/md'
 
 interface Props {
   ordenador:string,
@@ -19,7 +20,9 @@ const Ordenador = ({ordenador, setOrdenador}:Props) => {
             onBlur={()=> setAberto(false)}
        >
             <span>Ordenador ordena à dor </span>
-            
+
+            {aberto? <MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/>}
+
             <div className={classNames({
                [style.ordenador__options]:true,
                [style['ordenador__options--ativo']]:aberto
