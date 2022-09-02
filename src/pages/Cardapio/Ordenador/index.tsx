3afbb9282e99b9d2404b4060241b01b6c5e9e822
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-02 16:03:26 -0300

    ...

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -5,7 +5,7 @@ import opcoes from './opcoes.json'
 const Ordenador = () => {
   return(
        <button className={style.ordenador}>
-            <span>Ordenador ordena a dor </span>
+            <span>Ordenador ordena à dor </span>
             <div className={style.ordenador__options}>
                 {opcoes.map(opcao => (
                     <div className={style.ordenador__option} key={opcao.value}>
