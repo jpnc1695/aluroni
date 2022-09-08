11f96a4adc412d01e6f84e2a4d7963765fc8082b
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-08 16:17:02 -0300

    criação const rotas no componente menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,9 +2,30 @@ import {ReactComponent as Logo} from 'assets/logo.svg';
 import styles from './menu.module.scss'
 
 const Menu = () => {
+
+  const rotas =  [{
+     label: 'Início',
+     to: '/'
+  }, {
+    label: 'Cardapio',
+    to:'/cardapio'
+  },{
+    label:'Sobre',
+    to:'/sobre'
+  }];
+
   return(
      <div>
-       <nav className={styles.menu}> <Logo/> </nav>
+       <nav className={styles.menu}> 
+        <Logo/> 
+          <ul className={styles.menu__list}>
+            {rotas.map((rota, index)=> (
+              <li key={index} className={styles.menu__link}>
+                 <a href={rota.to}> {rota.label} </a>
+              </li>
+              ))}
+          </ul>
+       </nav>
      </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import Inicio from 'pages/Inicio';
 import Cardapio from 'pages/Cardapio';
+import Menu from 'components/Menu'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const AppRouter = () => {
  return(
   <Router>
+     <Menu/>
       <Routes>
         <Route path='/' element={<Inicio/>}/>
         <Route path='/cardapio' element={<Cardapio/>}/>
