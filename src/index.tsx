db434898cb90a91007661792c54a074aad8fc553
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-08 15:17:50 -0300

    intalação react dom e criação routes.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AppRouter from 'routes';
 import "normalize.css";
 import './index.css';
-import Cardapio from './pages/Cardapio';
-import Inicio from 'pages/Inicio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-   <Inicio/>
+   <AppRouter/>
   </React.StrictMode>
 );
 
