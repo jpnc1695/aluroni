8db5da2e322e597627d0c7db4c2db11a6901f9c0
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-08 14:37:22 -0300

    criação componentes pagina inicio

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,15 @@
+import cardapio from 'pages/Cardapio/Itens/itens.json'
+import styles from './Inicio.module.scss'
+
+export default function Inicio() {
+  return (
+    <section>
+      <h3>
+        Recomendações da cozinha
+      </h3>
+      <div>
+
+      </div>
+    </section>
+  );
+}
