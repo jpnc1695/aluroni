8736e13bc804db284b061198fd7bb30d593634d6
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-12 15:02:07 -0300

    assets de sobre e criação de html sobre

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -1,11 +1,40 @@
-import styles from './Sobre.module.scss'
+import styles from "./Sobre.module.scss";
+import stylesTema from "styles/Tema.module.scss";
+import casa from 'assets/sobre/casa.png';
+import massa1 from 'assets/sobre/massa1.png';
+import massa2 from 'assets/sobre/massa2.png';
 
-const Sobre = () =>{
+const Sobre = () => {
   return (
-      <section>
-        Sobre !!!
-      </section>
-  )
-}
+    <section>
+      <h3 className={stylesTema.titulo}> Sobre</h3>
+      <div className={styles.sobreNos}>
+          <img src={casa} alt="Casa Aluroni" />
+        <div className={styles.sobreNos__texto}>
+          <p>
+            Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa
+            Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos
+            pelos ingredientes tradicionais da culinária Italiana, frescos e de
+            excelente qualidade para que sua experiência seja ainda mais
+            intensa!
+          </p>
+          <p>
+            Também possuímos uma cardápio de carnes com muitas opções de acordo
+            com o seu gosto!
+          </p>
+          <p>
+            Para acompanhar as massas italianas, Aluroni possui uma reserva de
+            vinhos especiais, que harmonizam perfeitamente com o seu parto, seja
+            carne ou massa!
+          </p>
+        </div>
+           <div className={styles.imagens}>
+              <img src={massa1} alt="Prato de massa" />
+              <img src={massa2} alt="Prato de massa" />
+           </div>
+      </div>
+    </section>
+  );
+};
 
 export default Sobre;
