4dd6e5851d06cc6507683d68c47002fda435ce3c
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-12 16:01:11 -0300

    ....

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,10 @@
 import styles from './Footer.module.scss'
+import { ReactComponent as Logo } from 'assets/logo.svg';
 
 const Footer = () => {
   return (
      <footer className={styles.Footer}>
-
+      <Logo/>
      </footer>
   )
 }
