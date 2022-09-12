1fed37787d88eaef94c14a1fd8534fb5315fdb94
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-12 16:17:08 -0300

    ..

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,10 +1,16 @@
 import styles from './NotFound.module.scss'
+import {ReactComponent as NotFoundImg} from 'assets/not_found.svg'
+import classNames from 'classnames';
+import stylesTema from 'styles/Tema.module.scss'
 
 const NotFound = () => {
   return (
-      <section>
-
-      </section>
+      <div className={classNames({
+         [styles.container]: true,
+         [stylesTema.container]:true
+      })}>
+        <NotFoundImg/>
+      </div>
   )
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Sobre from 'pages/Sobre';
 import Menu from 'components/Menu'
 import PAginaPadrao from 'components/PaginaPadrao'
 import Footer from 'components/Footer';
+import NotFound from 'pages/NotFound';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const AppRouter = () => {
@@ -16,6 +17,7 @@ const AppRouter = () => {
               <Route index element ={<Inicio/>}/>  
               <Route path='cardapio' element={<Cardapio/>}/>
               <Route path='sobre' element={<Sobre/>}/>
+              <Route path='*' element={<NotFound/>}/>
             </Route> 
         </Routes>
           <Footer/>
