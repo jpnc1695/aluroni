cacca9d47630c59eab45894b6637aa994ef98004
jpnc1695 <joaopaulonunescosta@gmail.com>
2022-09-13 15:45:09 -0300

    onclick para item ir a pagina do prato.

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,13 +1,17 @@
 import style from './Item.module.scss'
 import Tags from 'components/Tags'
 import {Prato} from 'types/Prato'
+import { useNavigate } from 'react-router-dom';
 
 const Item = (props:Prato ) => {
 
-  const {title, description, photo} = props;
+  const {id, title, description, photo} = props;
+  const navigate = useNavigate()
   
   return (
-    <div className={style.item}>
+    <div className={style.item} onClick={()=>{
+      navigate(`/prato/${id}`)
+    }}>
         <div className={style.item__imagem}>
             <img src={photo} alt={title} />
         </div>
